ee4baeffe3283d41fd0201f299ccaa0abcd96aff
bhhoang818 <42143903+bhhoanggl@users.noreply.github.com>
2025-06-15 22:09:50 +0700

    add

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,44 +1,44 @@
 const fs = require('fs');
-const globby = require('globby');
+const { globby } = require('globby');
 const prettier = require('prettier');
 
 async function generateSitemap() {
-    const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
-    const pages = await globby([
-        'pages/**/*.tsx',
-        '!pages/_*.tsx',
-        '!pages/api',
-        '!pages/404.tsx',
-    ]);
+  const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
+  const pages = await globby([
+    'pages/**/*.tsx',
+    '!pages/_*.tsx',
+    '!pages/api',
+    '!pages/404.tsx',
+  ]);
 
-    const sitemap = `
+  const sitemap = `
     <?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${pages
-            .map((page) => {
-                const path = page
-                    .replace('pages', '')
-                    .replace('.tsx', '')
-                    .replace('/index', '');
-                const route = path === '/index' ? '' : path;
-                return `
+      .map((page) => {
+        const path = page
+          .replace('pages', '')
+          .replace('.tsx', '')
+          .replace('/index', '');
+        const route = path === '/index' ? '' : path;
+        return `
             <url>
               <loc>${`https://bhhoang.com${route}`}</loc>
               <changefreq>daily</changefreq>
               <priority>${route === '' ? '1.0' : '0.7'}</priority>
             </url>
           `;
-            })
-            .join('')}
+      })
+      .join('')}
     </urlset>
   `;
 
-    const formatted = prettier.format(sitemap, {
-        ...prettierConfig,
-        parser: 'html',
-    });
+  const formatted = prettier.format(sitemap, {
+    ...prettierConfig,
+    parser: 'html',
+  });
 
-    fs.writeFileSync('public/sitemap.xml', formatted);
+  fs.writeFileSync('public/sitemap.xml', formatted);
 }
 
 generateSitemap();
